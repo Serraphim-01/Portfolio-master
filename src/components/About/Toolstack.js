04cbb1ff5f5da3bb8cc49b04891e648c24349a51
Sky Immortal <ojeremiah721@gmail.com>
2024-11-13 21:50:08 +0100

    Updated

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -13,13 +13,7 @@ function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiWindows11 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
       <SiUnity />
